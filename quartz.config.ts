c6636bd62486c58a0ee020d61e665c5105875b92
unknown <bryandang10@gmail.com>
2024-01-10 14:31:52 -0800

    Quartz sync: Jan 10, 2024, 2:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🐱‍🚀 bryandang",
+    pageTitle: "bryandang",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
